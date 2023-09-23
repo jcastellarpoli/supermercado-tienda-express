5545e80a530f6282c4358292bcb547649173205e
jcastellarpoli <jcastellar@poligran.edu.co>
2023-09-23 13:49:48 -0500

    Reinicio proyecto angular

--- tienda-express/src/app/app.component.spec.ts
+++ tienda-express/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tienda-express-front'`, () => {
+  it(`should have as title 'tienda-express'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tienda-express-front');
+    expect(app.title).toEqual('tienda-express');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tienda-express-front app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tienda-express app is running!');
   });
 });
