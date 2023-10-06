9f3fa86e04aefd22a32b66bceda2fdd0cbf153ce
jcastellarpoli <jcastellar@poligran.edu.co>
2023-10-06 00:05:41 -0500

    Version Beta

--- tienda-express-backend/rutas/users.js
+++ tienda-express-backend/rutas/users.js
@@ -11,15 +11,6 @@ routes.post('/users/login', (req, res) => {
     let foundUser = null;
     let token;
 
-    // for(var i = 0; i < users.length; i++)
-    // {
-    //     if(users[i].email === reqUser.email && users[i].password === reqUser.password)
-    //     {
-    //         foundUser = users[i];
-    //         foundUser.password = "";            
-    //     }
-    // }
-
     _.each(users, (user, i) => {
         if(user.email == reqUser.email && user.password == reqUser.password)
         {
